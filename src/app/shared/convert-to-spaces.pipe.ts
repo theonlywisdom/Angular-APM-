2158494f481a89ca1b5b163e955daa9c8711e138
Wisdom Akwoyere Baah <50482182+theonlywisdom@users.noreply.github.com>
2020-01-25 12:21:11 +0000

    Added a custome pipe to the product code

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+// pipe decorator
+@Pipe ({
+  name: 'convertToSpaces' //the name of the pipe to reference in html
+})
+//custom pipe class to convert characters to spaces
+// which implements the pipeTransform interface
+export class ConvertToSpacesPipe implements PipeTransform {
+  //transform method for the ConvertToSpaces class
+  // which replaces a specified character with a space
+  transform (value: string, character: string): string{
+    return value.replace(character, ' ');
+  }
+}
