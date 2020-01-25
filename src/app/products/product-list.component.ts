4f6a763716050169d29a3dac1de7248198367cfd
Wisdom Akwoyere Baah <50482182+theonlywisdom@users.noreply.github.com>
2020-01-25 14:48:31 +0000

    Implemented the products filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,20 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    /* set the filteredProducts array to the filtered list of products
+    using the JS conditional operator to handle the possibility
+     of a empty, null or undefined string */
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 2,
@@ -35,6 +48,25 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
+  //set default properties for the performFilter method
+  constructor() {
+    //set the filtered products to the full list of products
+    // and the default list is set to 'cart'
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  /*the performFilter method starts by:
+  converting the filter criteria to lower case for a case insentive comparison
+  then return the filtered list of products
+  */
+ performFilter(filterBy: string): IProduct[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
+
+  //the toggleImage method to show and hide images
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
--- src/app/shared/convert-to-spaces.pipe.ts
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -7,8 +7,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 //custom pipe class to convert characters to spaces
 // which implements the pipeTransform interface
 export class ConvertToSpacesPipe implements PipeTransform {
-  //transform method for the ConvertToSpaces class
-  // which replaces a specified character with a space
+  /* transform method for the ConvertToSpaces class
+   which replaces a specified character with a space*/
   transform (value: string, character: string): string{
     return value.replace(character, ' ');
   }
