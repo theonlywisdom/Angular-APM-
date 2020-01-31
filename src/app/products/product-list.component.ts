b9a3ae22bf9bb65da0efdb5b303c31c61cac3f23
Wisdom Akwoyere Baah <50482182+theonlywisdom@users.noreply.github.com>
2020-01-31 23:34:16 +0000

    Injected the service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -25,35 +26,13 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15-gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2019",
-      "description": "Curved-claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
 
   //set default properties for the performFilter method
-  constructor() {
+  constructor(private productService: ProductService) {
     //set the filtered products to the full list of products
     // and the default list is set to 'cart'
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
+  //  this.listFilter = 'cart';
   }
 
   onRatingClicked(message: string): void {
@@ -76,6 +55,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
