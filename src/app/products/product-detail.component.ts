f660f133b52b82ca2a5bdf3dae3d0cca133e1d95
Wisdom Akwoyere Baah <50482182+theonlywisdom@users.noreply.github.com>
2020-02-13 16:56:17 +0000

    Passed the id parameter to the product route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { IProduct } from './product';
+import { from } from 'rxjs';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +12,34 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  //set activated route as a dependency by defining it
+  //as a parameter to the constructor function
+  //the ActivatedRoute instance is then injected into
+  //this component class
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //we're using the snapshot approach here because 
+    //don't expect the url to change
+    //we get the id from the route and store it in a local variable
+    //"let" defines a blockscoped variable
+    //because the parameter is provided as a string
+    //we add a plus sign (+) at the beginning
+    //the a plus sign (+) is a JS shortcut to
+    //convert the parameter string to numeric id
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+  
+    }
   }
 
 }
